2bdbf201d987d54f4c91594929427d9710ef069f
Lokesh Shah <118357743+Lokeshshah02@users.noreply.github.com>
2024-11-08 22:06:40 +0530

    toaster

--- chattsy/src/app/dashboard/page.tsx
+++ chattsy/src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
-import DashNav from '@/components/dashboard/DashNav'
-import React from 'react'
-import { authOptions, CustomSession } from '../api/auth/[...nextauth]/options'
-import { getServerSession } from 'next-auth'
+import DashNav from "@/components/dashboard/DashNav";
+import React from "react";
+import { authOptions, CustomSession } from "../api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
 export default async function dashboard() {
-  const session :CustomSession | null = await getServerSession(authOptions)
+  const session: CustomSession | null = await getServerSession(authOptions);
   return (
-    <DashNav name={session?.user?.name!} image={session?.user?.image ?? undefined}/>
-  )
+    <>
+    <DashNav
+      name={session?.user?.name!}
+      image={session?.user?.image ?? undefined}
+    />
+    </>
+  );
 }
--- chattsy/src/app/layout.tsx
+++ chattsy/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
 import SessionProvider from "@/providers/SessionProvider";
 
 const geistSans = localFont({
@@ -30,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Toaster richColors duration={10000}/>
           {children}
         </body>
       </SessionProvider>
